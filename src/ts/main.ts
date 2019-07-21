e2cc7ea1a6448d334e21be07ec427ed481115ba0
dobernike <dobernike@mail.ru>
2019-07-21 18:25:38 +0300

    Добавляет работу с API:загрузка данных на страницу

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -40,3 +40,48 @@ form.addEventListener("submit", evt => {
   overlay.classList.remove("modal-show");
   console.warn("Вы вошли!");
 });
+
+// Loading data from API
+const _apiBase: string = "https://api.punkapi.com/v2/";
+
+const getBeers = async (page: number) => {
+  const url = `beers?page=${page}&per_page=6`;
+  const res = await fetch(_apiBase + url);
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${url}` + `, received ${res.status}`);
+  }
+  const json = await res.json();
+  return json.map(_transformBeers);
+};
+
+const _transformBeers = (beer: any) => {
+  return {
+    img: beer.image_url,
+    name: beer.name,
+    tagline: beer.tagline,
+    abv: beer.abv,
+    brewed: beer.first_brewed,
+    description: beer.description
+  };
+};
+// Rerender DOM
+let data = getBeers(1);
+data.then(beers => {
+  const cards = document.querySelectorAll(".beer-card");
+  cards.forEach((card, index) => {
+    const beer = beers[index];
+    const img = card.querySelector(".beer-card__image");
+    img.src = beer.img;
+    const name = card.querySelector(".beer-card__name");
+    name.innerText = "name: " + beer.name;
+    const tagline = card.querySelector(".beer-card__tagline");
+    tagline.innerText = "tagline: " + beer.tagline;
+    const abv = card.querySelector(".beer-card__abv");
+    abv.innerText = "abv: " + beer.abv;
+    const brewed = card.querySelector(".beer-card__brewed");
+    brewed.innerText = "brewed: " + beer.brewed;
+    const description = card.querySelector(".beer-card__description");
+    description.innerText = "description: " + beer.description;
+  });
+});
+
