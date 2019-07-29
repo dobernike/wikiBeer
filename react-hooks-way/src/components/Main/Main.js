dfbc28804d057cc97eeeb181d9d387e63d5df4f2
dobernike <Dobernike@mail.ru>
2019-07-29 18:57:12 +0300

    Добавляет рендер n-количества объектов

--- react-hooks-way/src/components/Main/Main.js
+++ react-hooks-way/src/components/Main/Main.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import CardBeer from "./CardBeer/CardBeer";
 
 export default function Main() {
-  return (
-    <main className="main container">
-      <CardBeer />
-    </main>
-  );
+  const [countCards, setCountCards] = useState(6);
+
+  const cards = [];
+
+  for (let i = 0; i < countCards; i++) {
+    cards.push(<CardBeer />);
+  }
+
+  return <main className="main container">{cards}</main>;
 }
