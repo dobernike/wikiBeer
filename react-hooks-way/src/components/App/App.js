fb3fd8352b159bda0456222f5e8693dd0622b2ef
dobernike <Dobernike@mail.ru>
2019-07-29 18:34:46 +0300

    Добавляет каркас приложения

---
+++ react-hooks-way/src/components/App/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Main from "../Main/Main";
+import Modal from "../Modal/Modal";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+      <Modal />
+    </>
+  );
+}
+
+export default App;
