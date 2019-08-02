d1593f3b321a6219d4af9644cd32f4e0188f3eb7
dobernike <dobernike@mail.ru>
2019-08-02 11:48:19 +0300

    Исправляет неправильный адрес

--- react-hooks-way/src/service/getBeers.js
+++ react-hooks-way/src/service/getBeers.js
@@ -1,31 +1,33 @@
 const _apiBase = "https://api.punkapi.com/v2/";
 
 async function getBeers(currentPage, cardsPerPage) {
-  const url = `/beers?page=${currentPage}&per_page=${cardsPerPage}`;
+  const url = `beers?page=${currentPage}&per_page=${cardsPerPage}`;
   const res = await fetch(_apiBase + url);
+
   if (!res.ok) {
     throw new Error(`Could not fetch ${url}, received ${res.status}`);
   }
+
   const beersJSON = await res.json();
   const beers = beersJSON.map(_transformBeers);
   return beers;
 }
 
 // function handleBeers(beersJSON) {
-  // this.view.clearMain();
+// this.view.clearMain();
 
-  // beersJSON.then((beersJSON) => {
-    // const beers = beersJSON.map(_transformBeers);
-    // return beers;
-    // const beersNames = [];
-    // beers.forEach((beer: any) => {
-    // beersNames.push(beer.name);
-    // const card = this.view.createCard(beer);
-    // this.handleCache(card);
-    // });
+// beersJSON.then((beersJSON) => {
+// const beers = beersJSON.map(_transformBeers);
+// return beers;
+// const beersNames = [];
+// beers.forEach((beer: any) => {
+// beersNames.push(beer.name);
+// const card = this.view.createCard(beer);
+// this.handleCache(card);
+// });
 
-    // this.createSearch(beersNames);
-  // });
+// this.createSearch(beersNames);
+// });
 // }
 
 function _transformBeers(beer) {
